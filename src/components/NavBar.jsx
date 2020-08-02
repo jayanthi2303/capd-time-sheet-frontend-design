0125a67c6c0c3d2606091840c48fb068ba9940cc
Samikkannu <jayanthi.samikkannu@dxc.com>
2020-08-02 16:04:07 +0530

    New integrated code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,11 @@ const NavBar = () => {
     return (
         <React.Fragment>
             <nav className="navbar navbar-dark m-5 bg-dark">
-                <a className="navbar-brand" href="/ResourceDetails"> <span><FaUserAstronaut /><p>User details</p></span></a>
-                <a className="navbar-brand" href="/ProjectDetails">   <span><FaProjectDiagram /><p>Project details</p></span></a>
-                <a className="navbar-brand" href="/SkillDetails">   <span><FaProjectDiagram /><p>Skill details</p></span></a>
-                <a className="navbar-brand" href="/Test">   <span><FaProjectDiagram /><p>Manager Entry</p></span></a>
-                <a className="navbar-brand" href="/TimeSheetDetails">   <span><FaProjectDiagram /><p>Resource Entry</p></span></a>
+                <a className="navbar-brand" href="/ResourceDetails"> <span><FaUserAstronaut /><p>User Table</p></span></a>
+                <a className="navbar-brand" href="/ProjectDetails">   <span><FaProjectDiagram /><p>Project Table</p></span></a>
+                <a className="navbar-brand" href="/SkillDetails">   <span><FaProjectDiagram /><p>Skill Table</p></span></a>
+                {localStorage.getItem('isAdmin') === 'true' && <a className="navbar-brand" href="/ManagerForm">   <span><FaProjectDiagram /><p>Manager Time Sheet</p></span></a>}
+                <a className="navbar-brand" href="/ResourceForm">   <span><FaProjectDiagram /><p>Resource Time Sheet</p></span></a>
                 <a className="navbar-brand" href="/login"> {localStorage.getItem('login') == 'true' ? <span><FiLogOut /><p>Logout</p></span> : <span><FiLogIn /><p>Login</p></span>}</a>
             </nav>
         </React.Fragment>
